23e6bf99474d47fd1b0022c2f9d532de5bcbe427
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-13 13:44:03 +0200

    Created camps of the class and G&S

--- Classes/Curriculum/Location.php
+++ Classes/Curriculum/Location.php
@@ -4,7 +4,56 @@
 namespace Com\Iesebre\Dam2\simongonzalez\Curriculum;
 
 
-class Location
-{
+class Location{
+
+    /**
+     * Here will be the current ID of each location. To locate we use both ID and type.
+     * @var
+     */
+    private $id;
+
+    /**
+     * If it is a classroom, basement, etc.
+     * @var
+     */
+    private $type;
+
+    //
+    /// --- Getters and Setters ---
+    //
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+
 
 }
