316314d18460742bdb76257c75febbdc747b52f5
SimonGonzalezCepeda <simongonzalez@iesebre.com>
2015-10-13 13:38:43 +0200

    Created camps of class and G&S

--- Classes/Curriculum/Grade.php
+++ Classes/Curriculum/Grade.php
@@ -2,7 +2,58 @@
 
 namespace namespace Com\Iesebre\Dam2\simongonzalez\Curriculum;
 
-class Grade
-{
+/**
+ * Class Grade
+ * @package Com\Iesebre\Dam2\simongonzalez\Curriculum
+ */
+class Grade{
+    /**
+     * Number of the grade. Example 1st.
+     * @var
+     */
+    private $grade;
 
+    /**
+     *  Time of the current grade, it can bet morning or afternoon.
+     * @var
+     */
+    private $daytime;
+
+
+    //
+    ///  --- Getters and Setters ---
+    //
+
+
+    /**
+     * @return mixed
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+
+    /**
+     * @param mixed $grade
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDaytime()
+    {
+        return $this->daytime;
+    }
+
+    /**
+     * @param mixed $daytime
+     */
+    public function setDaytime($daytime)
+    {
+        $this->daytime = $daytime;
+    }
 }
